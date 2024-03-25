1e329c1a0b92ae9c6879aa9aa952cceb6a77f1e4
Vlada <48159402+VladaDanilova@users.noreply.github.com>
2024-03-25 08:27:28 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MIGRATION TEST - link',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A simple project description',
+    long_description=open('README.md').read(),
+    install_requires=['altair', 'pytz'], 
+    url='https://github.com/VladaDanilova/test-connectionSomeoneWS',
+    author='Vlada Danilova',
+    author_email='just.you177@gmail.com'
+)
